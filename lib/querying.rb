7404856646683bde0338a4697668d4477629cba8
William Pannell <lpann5@gmail.com>
2020-06-24 20:52:15 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,6 +30,11 @@ end
 
 def select_series_title_with_most_human_characters
   "Write your SQL query here"
+  "SELECT series.title
+  FROM series
+  JOIN books
+  ON series.id = books.series_id
+  
 end
 
 def select_character_names_and_number_of_books_they_are_in
