356a46a5f2f2274d983211d33ad7d0b3e607efa9
William Pannell <lpann5@gmail.com>
2020-06-24 20:34:58 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -3,4 +3,4 @@ CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name TEXT );
 CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT );
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER );
 CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER);
-CREATE TABLE charcter_books (id INTEGER PRIMARY KEY, character_id INTEGER, book_id INTEGER);
+CREATE TABLE charcter_books ( character_id INTEGER, book_id INTEGER);
