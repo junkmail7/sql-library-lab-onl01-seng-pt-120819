cded32062b0a7de988c0e84f1c7cb9df0c59d916
William Pannell <lpann5@gmail.com>
2020-06-24 20:42:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,7 +1,8 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "Write your SQL query here"
   "SELECT books.title, books.year
-  FROM
+  FROM books
+  WHERE books.id = series.book_id
   ORDER BY books.year"
 end
 
