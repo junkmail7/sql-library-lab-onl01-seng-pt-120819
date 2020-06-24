450f138a982c1585fc179cbf153620670b26d329
William Pannell <lpann5@gmail.com>
2020-06-24 20:47:18 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -10,7 +10,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "Write your SQL query here"
-  "SELECT character.name, character.motto
+  "SELECT characters.name, characters.motto
   FROM characters
   ORDER BY LENGTH(motto) DESC LIMIT 1"
 end
