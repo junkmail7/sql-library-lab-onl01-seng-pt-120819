85b154494a4d9fe449a7b198519ecc93c0838920
William Pannell <lpann5@gmail.com>
2020-06-24 20:55:30 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,7 +39,8 @@ def select_series_title_with_most_human_characters
   JOIN characters
   ON characters.id = character_books.character_id
   WHERE characters.species = 'Human'
-  
+  GROUP BY series.title
+  ORDER BY COUNT (*) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
